ad4d8540ba248912a751f165268c57e3ce8dea30
kimjg9092 <84084547+kimjg9092@users.noreply.github.com>
2024-11-11 16:40:02 +0900

    Update web.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8001;
 
 app.use(express.json());
 
